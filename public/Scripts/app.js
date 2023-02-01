17b2db977e941d124bffe0646ffcad0263c96429
jjtan1996 <jasperjantan1996@gmail.com>
2023-02-01 17:07:56 -0500

    made some changes

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -7,4 +7,12 @@
     }
 
     window.addEventListener("load", Start);
-})();
+})()   
+
+function getDetails(){
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/; //for email validation
+
+    var name = document.getElementById('userField').value;
+    var email = document.getElementById('emailField').value;
+    var inquiry = document.getElementById('inquiryField').value;    
+}
